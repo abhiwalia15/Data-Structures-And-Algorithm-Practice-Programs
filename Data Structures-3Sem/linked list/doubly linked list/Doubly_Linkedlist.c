d36eabc717d1fb50edf4a4978b97a95350952263
Mrinal <waliamrinal@gmail.com>
2019-02-27 03:23:55 +0530

    doubly linked list and circular doubly linked list operations

---
+++ Data Structures-3Sem/linked list/doubly linked list/Doubly_Linkedlist.c
@@ -0,0 +1,178 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+struct node {
+    int info;
+    struct node *llink;
+    struct node *rlink;
+    };
+
+typedef struct node *NODE;
+    NODE first=NULL;
+    NODE last=NULL;
+
+void insertfront()
+{
+   NODE temp = (NODE)malloc(sizeof(struct node));
+        printf("Enter the data\n");
+        scanf("%d",&temp->info);
+        temp->llink = NULL;
+        temp->rlink = NULL;
+
+        if(first==NULL) {
+            first=last=temp;
+            return;
+        }
+
+        else {
+                temp->rlink=first;
+                first->llink=temp;
+                first=temp;
+        }
+}
+
+void insertend()
+{
+   NODE temp = (NODE)malloc(sizeof(struct node));
+        printf("Enter the data\n");
+        scanf("%d",&temp->info);
+        temp->llink = NULL;
+        temp->rlink = NULL;
+
+        if(first==NULL) {
+            first=last=temp;
+            return;
+        }
+
+        else {
+                temp->llink=last;
+                last->rlink=temp;
+                last=temp;
+        }
+}
+
+void deletebeg()
+{
+    NODE temp;
+    if(first==NULL)
+    {
+        printf("List Empty\n");
+        return;
+    }
+
+    else if(first->rlink==NULL)
+    {
+        printf("%d is deleted\n",first->info);
+        free(first);
+        first=NULL;
+    }
+
+    else
+    {
+        temp=first;
+        first=first->rlink;
+        first->llink=NULL;
+        printf("%d is deleted\n",temp->info);
+        free(temp);
+        temp=NULL;
+    }
+}
+
+void deleteend()
+{
+    NODE temp;
+    if(first==NULL)
+    {
+        printf("List Empty\n");
+        return;
+    }
+
+    else if(first->rlink==NULL)
+    {
+        printf("%d is deleted\n",first->info);
+        free(first);
+        first=NULL;
+    }
+
+    else
+    {
+        temp=last;
+        last=last->llink;
+        last->rlink=NULL;
+        printf("%d is deleted\n",temp->info);
+        free(temp);
+        temp=NULL;
+    }
+}
+
+void display()
+{
+    NODE cur;
+
+    if(first==NULL)
+    {
+        printf("LIst EMPTY\n");
+        return;
+    }
+
+    else
+    {
+        cur=first;
+        printf("THE LIST IS\n");
+        while(cur!=NULL)
+        {
+            printf("%d\t",cur->info);
+            cur=cur->rlink;
+
+        }
+    }
+}
+
+void main()
+{
+    int ch;
+
+    printf("1.insertfront\t2.insertend\t3.deletefront\t4.deleteend\t5.display\t6.exit\n");
+    for(;;)
+    {
+        printf("Enter your choice\n");
+        scanf("%d",&ch);
+
+        switch(ch)
+        {
+            case 1: insertfront();
+                break;
+            case 2: insertend();
+                break;
+            case 3: deletebeg();
+                break;
+            case 4: deleteend();
+                break;
+            case 5:display();
+                break;
+            case 6:exit(0);
+                break;
+
+        }/*end switch*/
+    }/*end for*/
+}/*end main*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
