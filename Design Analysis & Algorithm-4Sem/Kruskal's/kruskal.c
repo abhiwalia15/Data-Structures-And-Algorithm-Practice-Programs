1cdd2f337efda897aae5d0cb5e65228c67a1519a
Mrinal <waliamrinal@gmail.com>
2019-03-13 03:05:16 +0530

    kruskals lab program

---
+++ Design Analysis & Algorithm-4Sem/Kruskal's/kruskal.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+struct Edge
+{
+    int cost, begv, endv;
+}; typedef struct Edge E;
+
+void main()
+{
+    int i, j, a[10][10], n;
+
+    printf("Enter the no. of nodes\n");
+    scanf("%d",&n);
+
+    printf("Enter the adj. matrix\n");
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+
+            kruskal(a,n);
+
+}/*end main*/
+
+kruskal(int a[10][10], int n)
+{
+    E Ed[100], temp;
+    int i, j, count=1;
+
+    /*dont accept 0 and 999's*/
+    for(i=1;i<=n;i++)
+        for(j=1;j<=n;j++)
+            if(a[i][j]!=0 && a[i][j]!=999)
+    {
+
+        Ed[count].begv = i;
+        Ed[count].endv = j;
+        Ed[count].cost = a[i][j];
+        count++;
+    }/* end for*/
+
+    count = count-1;
+
+    /* arrange in ascending order now*/
+    for(i=1;i<=count-1;i++)
+        for(j=1;j<=count-i;j++)
+        if(Ed[j+1].cost < Ed[j].cost)
+    {
+        temp = Ed[j+1];
+        Ed[j+1] = Ed[j];
+        Ed[j] = temp;
+    }/*end for*/
+
+
+}
+
